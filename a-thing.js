833ad16bb4bb34532876ff0eff13688b892dc26d
uhhithere <129226914+uhhithere@users.noreply.github.com>
2023-03-28 14:39:22 -0700

    it's a-thing.js

--- a-thing.js
+++ a-thing.js
@@ -1,1 +1,61 @@
-console.log("welcome to a-thing.js!!")
+alert("welcome to a-thing.js!!")
+if (confirm("do you want to play a-thing.js?")) {
+    alert("yay!! lets play!!")
+    alert("hi, "+prompt("what is your name")+". i'm sure we will become  b e s t   f r i e n d s .")
+    if (confirm("would you like to die")) {
+        alert("wow um ok you just like willingly die???? you need help i cana reccomend you to a good therapist")
+    } else {
+        alert("too bad loser")
+        alert("just kidding ha ha")
+        if (prompt('TYPE "BOOGIE" TO DANCE A LOT').toLowerCase=="boogie") {
+            alert("you dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("and dance")
+            alert("you take a break")
+            alert("then dance some more")
+            alert("then")
+            alert("you")
+            alert("DIE")
+            alert("you got dance 'till you die ending")
+        } else {
+            alert("good. you would've died. trust me")
+            if (confirm("click cancel or die")) {
+                alert("ha trick question you didn't die")
+                alert("don't try to die")
+                alert("please")
+                if (prompt("don't you dare type labrador").toLowerCase=="labrador") {
+                    alert("what have you done")
+                    alert("we are doomed")
+                    alert("you got the you doomed us all ending")
+                } else {
+                    alert("thanks")
+                    if (confirm("click either button")) {
+                        alert("YOU WIN ASDFGHJKL")
+                    } else {
+                        alert("you suck")
+                    }
+                }
+            } else {
+                alert("ha ha trick question you died sucker")
+                alert("you got the cancel ending")
+            }
+        }
+    }
+} else {
+    alert("coward")
+}
